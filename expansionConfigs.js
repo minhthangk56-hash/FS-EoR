988f28fc7ae2a9e98465e90e08ca47c2884930b6
SirConnery <jaakkovallinkoski@gmail.com>
2026-09-05 10:09:18 +0300

    Init

---
+++ expansionConfigs.js
@@ -0,0 +1,15 @@
+const expansions = [
+  {
+    expansionName: "Echo of Ruination",
+    expansionColor: "navy",
+    factionNames: [
+      "Dark Mechanicum",
+      "Imperial Knight",
+    ],
+    factionFolders: [
+      "DarkMechanicum",
+      "ImperialKnight",
+    ],
+    factionColor: ["maroon", "cadetblue",],
+  },
+];
